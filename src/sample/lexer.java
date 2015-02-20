9d54873e151a22cbac720035e4cd7a1c99ee27e0
Matthew Musich <mcr1221@hotmail.com>
2015-02-20 00:22:31 -0500

    Setting up the lexer portion

--- src/sample/lexer.java
+++ src/sample/lexer.java
@@ -1,11 +1,22 @@
 package sample;
 
+import java.util.Dictionary;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by matt on 2/9/2015.
  */
 public class lexer {
 
+    
 
+    public String lexercise(){
+
+
+
+        return "This will be the Array output of tokens";
+    }
 
 
 }
