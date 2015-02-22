e0e32b573d22e2030c31183292d81071693a2304
Matthew Musich <mcr1221@hotmail.com>
2015-02-22 01:20:45 -0500

    My efforts today seemed useless...
    As a reminder to myself tomorrow, figure out how to use a pattern &
    match system. cause if else's are the bane of my exsistance

--- src/sample/lexer.java
+++ src/sample/lexer.java
@@ -1,41 +1,104 @@
 package sample;
 
-import java.util.Arrays;
-import java.util.Dictionary;
-import java.util.HashSet;
-import java.util.Set;
+
+import java.util.*;
 import java.util.regex.*;
 import java.lang.Enum;
 
 
 public class lexer {
 
-    public static enum TokenType
 
 
+    // This will loop through input to determine tokens
     public static String lexercise(String baseString){
 
-        String newInput = baseString;
+        ArrayList<token> tokens = new ArrayList<token>();
 
+        // MY Strings
+        String pastText = "";
+        String remainingText = baseString;
+        String currentText = "";
+        String tokenText = "";
+        String errorText = "Errors: ";
+        String tokenString = "";
+        int forward = 1;
 
+        for(int i=0; i < remainingText.length(); i++) {
+            if (Character.toString(baseString.charAt(i)).matches("[a-z]") && Character.toString(baseString.charAt(i+1)).matches("[a-z]")) {
+                switch ((baseString.charAt(i))) {
+                    case 'b': if (Character.toString(baseString.charAt(i+1)).matches("o")){
+                                if (Character.toString(baseString.charAt(i+1)).matches("o")){
+                                    if (Character.toString(baseString.charAt(i+1)).matches("l")){
+                                        if (Character.toString(baseString.charAt(i+1)).matches("e")){
+                                            if (Character.toString(baseString.charAt(i+1)).matches("a")){
+                                                if (Character.toString(baseString.charAt(i+1)).matches("n")){
+                                                    tokens.add(new token("type","boolean"));
+                                                 }
+                                            }
+                                        }
+                                    }
+                                }
+                              } else { errorText += "Error at position: " + String.valueOf(i) + " Nonvaild Character";}
+                            break;
+                    case 'f':
+                            break;
+                    case 'i':
+                            break;
+                    case 'p':
+                            break;
+                    case 's':
+                            break;
+                    case 't':
+                            break;
+                    case 'w':
+                            break;
+                    default:
+                            break;
 
+                }
 
+            } else if(Character.toString(baseString.charAt(i)).matches("[a-z]")) {
 
-        for(int i=0; i < baseString.length(); i++){
-            if (Character.toString(baseString.charAt(i)).matches("[a-z]") ) {
-                if (Character.toString(baseString.charAt(i)).matches("b"))
-                    tempLetters += Character.toString(baseString.charAt(i));
+            } else if (Character.toString(baseString.charAt(i)).matches("[0-9]")) {
 
-
-
-            } else {
+            } else if (Character.toString(baseString.charAt(i)).matches("[{|}|(|)|\"|=|==|!=]")) {
 
             }
+
         }
 
 
-        String lexedString = "";
 
+
+/*
+        for(int i=0; i < remainingText.length(); i++) {
+            if (Character.toString(baseString.charAt(i)).matches("b")) {
+                Pattern p = Pattern.compile("boolean");
+                Matcher m = p.matcher(remainingText);
+                if (m.find()) {
+                    tokens.add(new token("type", "boolean"));
+                } else {
+
+                }
+            }
+
+
+
+        }
+*/
+
+
+
+
+
+        //output management
+        if (!tokens.isEmpty()) {
+            for (int x=0; x < tokens.size(); x++)
+            tokenString = tokens.get(x).getToken();
+        }
+
+        String lexedString = errorText + "\n" + tokenString;
         return lexedString;
     }
 
--- src/sample/token.java
+++ src/sample/token.java
@@ -2,30 +2,78 @@ package sample;
 
 
 public class token {
+// EW JUST EW WHY DID I DO THIS
+//    // Declare all of the REGEX typing with the enums
+//    public static enum TokenType {
+//
+//        //keywords
+//        BOOL("boolean"),
+//        FALSE("false"),
+//        IF("if"),
+//        INT("int"),
+//        PRINT("print"),
+//        STRING("string"),
+//        TRUE("true"),
+//        WHILE("while"),
+//
+//        //variable
+//        VAR("[a-z]"),
+//
+//        //Numbers
+//        NUMBER("[0-9]"),
+//
+//        //Operations
+//        LBRACK("{"),
+//        RBRACK("}"),
+//        LPAREN("("),
+//        RPAREN(")"),
+//        EQUALS("="),
+//        DUBEQUALS("=="),
+//        NOTEQUALS("!="),
+//        PLUS("+"),
+//
+//        WHITESPACE("[ \t\f\r\n]+"),
+//        SYMBOL("[{|}|(|)");
+//
+//
+//        public final String pattern;
+//
+//        private TokenType(String pattern) {
+//            this.pattern = pattern;
+//        }
+//
+//    };
 
-    private String tokenType;
-    private String tokenData;
+    public String type;
+    public String data;
 
+    //CONSTRUCTOR
     public token (String type, String data){
-        setTokenType(type);
-        setTokenData(data);
+        this.type = type;
+        this.data = data;
     }
 
+
+    //not needed for now
     public void setTokenType(String x) {
-        tokenType = x;
+        type = x;
     }
 
     public void setTokenData(String x){
-        tokenData = x;
+        data = x;
     }
 
+
+
+
     public String getTokenType() {
-        return tokenType;
+        return type;
     }
 
     public String getTokenData() {
-        return tokenData;
+        return data;
     }
+
 
     public String getToken(){
         return "(" + getTokenType()  + "," + getTokenData() + ")";
